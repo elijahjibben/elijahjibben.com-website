c406ccf44ad5f6daf070116795259ed524bf25c1
Elijah Jibben <elijah.jibben@gmail.com>
2024-10-12 22:17:43 -0700

    Added images

--- app/page.tsx
+++ app/page.tsx
@@ -81,8 +81,9 @@ function renderDisplayedText2() {
                 style={{ height: '100%', paddingLeft: 20, paddingRight: 20 }}
               >
                 <Image
-                  src='../images/turtle_icon.png'
+                  src="https://github.com/elijahjibben/mantine-testing/blob/master/images/turtle_icon.png?raw=true"
                   alt="Logo"
+                  radius={"xl"}
                   width={40}
                   height={40}
                 />
