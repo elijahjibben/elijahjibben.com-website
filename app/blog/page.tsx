9c9e9a7900f4b1f4638783c0755bb769fbd71268
Elijah Jibben <elijah.jibben@gmail.com>
2024-10-13 17:14:19 -0700

    Updated spacing

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,9 @@
-// src/pages/BlogPage.tsx
 import React from 'react';
 import HeaderComponent from '../components/Header/Header';
 
 const BlogPage: React.FC = () => {
   return (
-    <div>
+    <div style={{ paddingTop: '50px' }}>
       <HeaderComponent />
       <h1>Blog Page</h1>
       <p>This is where blog content will go.</p>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,10 +3,12 @@
 
 import React from 'react';
 import { CardsCarousel } from '../components/Carousels/CardsCarousel';
+import HeaderComponent from '../components/Header/Header';
 
 const ProjectsPage: React.FC = () => {
   return (
-    <div>
+    <div style={{ paddingTop: '50px' }}>
+      <HeaderComponent/>
       <h1>Projects Page</h1>
       <p>This is where project content will go.</p>
     </div>
