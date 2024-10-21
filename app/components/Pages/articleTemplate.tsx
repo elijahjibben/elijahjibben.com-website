df7b983a98b6e17ebac7f942806860feb7cce9a7
Elijah Jibben <elijah.jibben@gmail.com>
2024-10-21 16:38:50 -0700

    Updated styling on article pages

--- app/components/Pages/articleTemplate.tsx
+++ app/components/Pages/articleTemplate.tsx
@@ -3,10 +3,11 @@
 'use client'
 
 import HeaderComponent from "@/app/components/Header/Header";
-import { TypographyStylesProvider } from "@mantine/core";
+import { Paper, TypographyStylesProvider } from "@mantine/core";
 import { marked } from "marked";
 import { useEffect, useState } from "react";
 import '../../styles/articleStyles.css';
+import FooterComponent from "../common/Footer";
 
 interface ArticleTemplateProps {
   markdownPath: string; // Accept the markdown file path as a prop
@@ -37,11 +38,14 @@ export default function ArticleTemplate({ markdownPath }: ArticleTemplateProps) 
       <div>
         <HeaderComponent />
       </div>
-      <div style={{ margin: '0 auto', padding: '20px', paddingTop: '80px', maxWidth: '1000px' }}>
+      <div style={{ margin: '0 auto', padding: '20px', paddingTop: '80px', maxWidth: '1500px' }}>
+      <Paper shadow="xl" radius="lg" p="lg" withBorder style={{ backgroundColor: '#DBD6C7' }}>
         <TypographyStylesProvider>
           <div className="article-content" dangerouslySetInnerHTML={{ __html: html }} />
         </TypographyStylesProvider>
+        </Paper>
       </div>
+      <div><FooterComponent /></div>
     </>
   );
 }
